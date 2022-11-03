18442d900fccef5cc0307b55ceeb19dab16bb270
Seher kanwal <seherkanwal223@gmail.com>
2022-11-04 04:33:12 +0500

    Initial commit

---
+++ app/src/main/java/com/example/workingcalculator/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.workingcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void add_Number(View v){
+
+        // here we just mapping the our ui with the variables
+        EditText num1 = (EditText)findViewById(R.id.num1);
+        EditText num2 = (EditText) findViewById(R.id.num2);
+        EditText res = (EditText) findViewById(R.id.result);
+
+       // here we are getting the data or value from the text fields
+        int n1 = Integer.parseInt(num1.getText().toString());
+        int n2= Integer.parseInt(num2.getText().toString());
+        int result = n1 + n2;
+        res.setText("The Sum is :" + result);
+    }
+    public void Subtract_Number(View v){
+        EditText num1 = (EditText) findViewById(R.id.num1);
+        EditText num2 = (EditText) findViewById(R.id.num2);
+        EditText res = (EditText) findViewById(R.id.result);
+
+        // here we are getting the data or value from the text fields
+        int n1 = Integer.parseInt(num1.getText().toString());
+        int n2= Integer.parseInt(num2.getText().toString());
+        int result = n1 - n2;
+        res.setText("The Subtract is :" + result);
+    }
+    public void multiple_Number(View v){
+        EditText num1 = (EditText) findViewById(R.id.num1);
+        EditText num2 = (EditText) findViewById(R.id.num2);
+        EditText res = (EditText) findViewById(R.id.result);
+
+        // here we are getting the data or value from the text fields
+        int n1 = Integer.parseInt(num1.getText().toString());
+        int n2= Integer.parseInt(num2.getText().toString());
+        int result = n1 * n2;
+        res.setText("The Subtract is :" + result);
+    }
+}
