7a9c9df6eaf7b16ce46ecf22faf0be687c8cb703
Seher Kanwal <92606737+Seher-Kanwal@users.noreply.github.com>
2022-11-23 06:07:25 +0500

    add image button

--- app/src/main/java/com/example/workingcalculator/MainActivity.java
+++ app/src/main/java/com/example/workingcalculator/MainActivity.java
@@ -13,7 +13,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public  void divideNumber(View v)
+    {
+        EditText num1 = (EditText)findViewById(R.id.num1);
+        EditText num2 = (EditText) findViewById(R.id.num2);
+        EditText result = (EditText) findViewById(R.id.result);
 
+        int n1 = Integer.parseInt(num1.getText().toString());
+        int n2 = Integer.parseInt(num2.getText().toString());
+        int result1 = n1 /n2;
+        result.setText("Answer is : "+ result1);
+    }
     public void add_Number(View v){
 
         // here we just mapping the our ui with the variables
@@ -47,6 +57,6 @@ public class MainActivity extends AppCompatActivity {
         int n1 = Integer.parseInt(num1.getText().toString());
         int n2= Integer.parseInt(num2.getText().toString());
         int result = n1 * n2;
-        res.setText("The Subtract is :" + result);
+        res.setText("The Answer is :" + result);
     }
 }
